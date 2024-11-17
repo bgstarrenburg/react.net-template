33d18fa34d8211205dc307816784a838ddd6c997
Ben <0984798@hr.nl>
2024-11-17 16:56:24 +0100

    Initial project setup

---
+++ Backend/Main/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace Main.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
---
+++ Frontend/spa/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "../../Backend/Main/wwwroot/js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|tsx|ts)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts"],
+  },
+};
